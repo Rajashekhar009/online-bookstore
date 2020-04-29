3663db6a7a7fd0f246c236a61703edc2b80dd9d1
Rajashekhar.Anumalla <shekaranumalla@gmail.com>
2020-04-29 23:40:31 +0200

    Initial Commit with created spring boot project

--- onlinebookstore/src/main/java/com/shekhar/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/com/shekhar/onlinebookstore/OnlineBookStoreApplication.java
@@ -2,7 +2,7 @@ package com.shekhar.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
